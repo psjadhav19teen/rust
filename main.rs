a96f4ec501cc992f8ddbf3678cfb37e66a992465
psjadhav19teen <42905297+psjadhav19teen@users.noreply.github.com>
2023-06-05 11:41:24 +0530

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,17 +1,36 @@
-mod userinput;
 
-fn main() {
-    println!("Hello, Everyone!");
-    userinput::show1();
-    userinput::show2();
-    userinput::register();
-    userinput::datatypes();
-    let arr=[11,23,24];
-    println!("arr[0]={}",arr[0]);
-    println!("All elements of arr={:?}",arr);
-    for i in 0..arr.len()
+fn main() 
+{
+    struct Student
     {
-        println!("{}",arr[i]);
+        name:String,
+        rollno:u16,
+        grade:char,
     }
+let s1=Student
+{
+    name:String::from("aaa"),
+    rollno:123,
+    grade:'A',
+};
+let s2=Student
+{
+    name:String::from("bbb"),
+    rollno:456,
+    grade:'O',
+};
+println!("name={}",s1.name);
+println!("rollno={}",s1.rollno);
+println!("grade={}",s1.grade);
+
+fn show(s2:Student)
+{
+println!("name={}",s2.name);
+println!("rollno={}",s2.rollno);
+println!("grade={}",s2.grade);
 }
+show(s2);
+}
+
+
 
